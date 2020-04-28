d16af79e968dd416981be5f167462ee68fe2e67e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-28 16:27:24 +0430

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ff-cl-gen
-OpenCL code generator for finite-field arithmetic over arbitrary prime fields
+# ff-cl-gen [![Crates.io](https://img.shields.io/crates/v/ff-cl-gen.svg)](https://crates.io/crates/ff-cl-gen)
+
+OpenCL code generator for finite-field arithmetic over arbitrary prime fields.
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use ff::PrimeField;
+use itertools::join;
+
+static FIELD_SRC: &str = include_str!("cl/field.cl");
+
+/// Divide anything into 64bit chunks
+fn limbs_of<T>(value: &T) -> &[u64] {
+    unsafe {
+        std::slice::from_raw_parts(
+            value as *const T as *const u64,
+            std::mem::size_of::<T>() / 8,
+        )
+    }
+}
+
+/// Calculate the `INV` parameter of Montgomery reduction algorithm for 64bit limbs
+/// * `a` - Is the first limb of modulus
+fn calc_inv(a: u64) -> u64 {
+    let mut inv = 1u64;
+    for _ in 0..63 {
+        inv = inv.wrapping_mul(inv);
+        inv = inv.wrapping_mul(a);
+    }
+    return inv.wrapping_neg();
+}
+
+fn params<F>(name: &str) -> String
+where
+    F: PrimeField,
+{
+    let one = F::one();
+    let one = limbs_of(&one); // Get Montomery from of F::one()
+    let p = F::char();
+    let p = limbs_of(&p); // Get regular form of field modulus
+    let limbs = one.len(); // Number of limbs
+    let inv = calc_inv(p[0]);
+    let limbs_def = format!("#define {}_LIMBS {}", name, limbs);
+    let p_def = format!(
+        "#define {}_P (({}){{ {{ {} }} }})",
+        name,
+        name,
+        join(p, ", ")
+    );
+    let one_def = format!(
+        "#define {}_ONE (({}){{ {{ {} }} }})",
+        name,
+        name,
+        join(one, ", ")
+    );
+    let zero_def = format!(
+        "#define {}_ZERO (({}){{ {{ {} }} }})",
+        name,
+        name,
+        join(vec![0u32; limbs], ", ")
+    );
+    let inv_def = format!("#define {}_INV {}", name, inv);
+    let typedef = format!("typedef struct {{ limb val[{}_LIMBS]; }} {};", name, name);
+    return format!(
+        "{}\n{}\n{}\n{}\n{}\n{}",
+        limbs_def, one_def, p_def, zero_def, inv_def, typedef
+    );
+}
+
+fn field_add_sub<F>(name: &str, is_sub: bool) -> String
+where
+    F: PrimeField,
+{
+    let op = if is_sub { "sub" } else { "add" };
+    let one = F::one();
+    let len = limbs_of(&one).len();
+    let mut src = String::from(format!(
+        "{} {}_{}_({} a, {} b) {{\n",
+        name, name, op, name, name
+    ));
+
+    if len > 1 {
+        src.push_str("asm(");
+        src.push_str(format!("\"{}.cc.u64 %0, %0, %{};\\r\\n\"\n", op, len).as_str());
+        for i in 1..len - 1 {
+            src.push_str(
+                format!("\"{}c.cc.u64 %{}, %{}, %{};\\r\\n\"\n", op, i, i, len + i).as_str(),
+            );
+        }
+        src.push_str(
+            format!(
+                "\"{}c.u64 %{}, %{}, %{};\\r\\n\"\n",
+                op,
+                len - 1,
+                len - 1,
+                2 * len - 1
+            )
+            .as_str(),
+        );
+        src.push_str(":");
+        let inps = join((0..len).map(|n| format!("\"+l\"(a.val[{}])", n)), ", ");
+        src.push_str(inps.as_str());
+
+        src.push_str("\n:");
+        let outs = join((0..len).map(|n| format!("\"l\"(b.val[{}])", n)), ", ");
+        src.push_str(outs.as_str());
+        src.push_str(");\n");
+    }
+
+    src.push_str("return a;\n}\n");
+
+    return src;
+}
+
+pub fn field<F>(name: &str) -> String
+where
+    F: PrimeField,
+{
+    return format!(
+        "{}\n{}\n{}\n{}\n",
+        params::<F>(name),
+        field_add_sub::<F>(name, false),
+        field_add_sub::<F>(name, true),
+        String::from(FIELD_SRC).replace("FIELD", name)
+    );
+}
+
+
+#[cfg(test)]
+mod tests {
+}
