42cb3ecd5b46b4a4e59dc263c2b8f16f2c5ee31e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-07-20 18:16:56 +0430

    + -> =

--- src/amd.rs
+++ src/amd.rs
@@ -10,46 +10,45 @@ where
     let mut result = String::new();
 
     if L::bits() == 32 {
-    result.push_str("#ifdef AMD\n");
-    for op in &["SUB", "ADD"] {
-        let len = L::limbs_of(F::one()).len();
+        result.push_str("#ifdef AMD\n");
+        for op in &["SUB", "ADD"] {
+            let len = L::limbs_of(F::one()).len();
 
-        let mut src = format!("FIELD FIELD_{}_amd(FIELD a, FIELD b) {{\n", op);
-        if len > 1 {
-            src.push_str("__asm volatile(");
-            src.push_str(format!("\"S_{}_U32 %0, %0, %{};\\n\"\n", op, len).as_str());
-            for i in 1..len {
-                src.push_str(
-                    format!(
-                        "\"S_{}_U32 %{}, %{}, %{};\\n\"\n",
-                        if *op == "ADD" {"ADDC"} else {"SUBB"},
-                        i,
-                        i,
-                        len + i
-                    )
-                    .as_str(),
+            let mut src = format!("FIELD FIELD_{}_amd(FIELD a, FIELD b) {{\n", op);
+            if len > 1 {
+                src.push_str("__asm volatile(");
+                src.push_str(format!("\"S_{}_U32 %0, %{}, %{};\\n\"\n", op, len, 2 * len).as_str());
+                for i in 1..len {
+                    src.push_str(
+                        format!(
+                            "\"S_{}_U32 %{}, %{}, %{};\\n\"\n",
+                            if *op == "ADD" { "ADDC" } else { "SUBB" },
+                            i,
+                            len + i,
+                            2 * len + i
+                        )
+                        .as_str(),
+                    );
+                }
+                src.push_str(":");
+                let inps = join((0..len).map(|n| format!("\"=s\"(a.val[{}])", n)), ", ");
+                src.push_str(inps.as_str());
+
+                src.push_str("\n:");
+                let outs = join(
+                    (0..len)
+                        .map(|n| format!("\"s\"(a.val[{}])", n))
+                        .chain((0..len).map(|n| format!("\"s\"(b.val[{}])", n))),
+                    ", ",
                 );
+                src.push_str(outs.as_str());
+                src.push_str(");\n");
             }
-            src.push_str(":");
-            let inps = join(
-                (0..len).map(|n| format!("\"+s\"(a.val[{}])", n)),
-                ", ",
-            );
-            src.push_str(inps.as_str());
+            src.push_str("return a;\n}\n");
 
-            src.push_str("\n:");
-            let outs = join(
-                (0..len).map(|n| format!("\"s\"(b.val[{}])", n)),
-                ", ",
-            );
-            src.push_str(outs.as_str());
-            src.push_str(");\n");
+            result.push_str(&src);
         }
-        src.push_str("return a;\n}\n");
-
-        result.push_str(&src);
-    }
-    result.push_str("#endif\n");
+        result.push_str("#endif\n");
     }
 
     result
