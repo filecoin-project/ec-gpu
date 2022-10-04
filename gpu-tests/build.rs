ec522dfff9d72e0551fd47c4a40f911508f43fda
Volker Mische <volker.mische@gmail.com>
2022-10-04 16:27:44 +0200

    WIP: use pasta curves instead of bls

--- gpu-tests/build.rs
+++ gpu-tests/build.rs
@@ -3,12 +3,14 @@ fn main() {}
 
 #[cfg(any(feature = "cuda", feature = "opencl"))]
 fn main() {
-    use blstrs::{Fp, Fp2, G1Affine, G2Affine, Scalar};
+    //use blstrs::{Fp, Fp2, G1Affine, G2Affine, Scalar};
     use ec_gpu_gen::SourceBuilder;
+    use pasta_curves::{EpAffine, Fp};
 
-    let source_builder = SourceBuilder::new()
-        .add_fft::<Scalar>()
-        .add_multiexp::<G1Affine, Fp>()
-        .add_multiexp::<G2Affine, Fp2>();
+    //let source_builder = SourceBuilder::new()
+    //    .add_fft::<Scalar>()
+    //    .add_multiexp::<G1Affine, Fp>()
+    //    .add_multiexp::<G2Affine, Fp2>();
+    let source_builder = SourceBuilder::new().add_multiexp::<EpAffine, Fp>();
     ec_gpu_gen::generate(&source_builder);
 }
--- gpu-tests/tests/multiexp.rs
+++ gpu-tests/tests/multiexp.rs
@@ -13,6 +13,7 @@ use ff::{Field, PrimeField};
 use group::Curve;
 use group::{prime::PrimeCurveAffine, Group};
 use pairing::Engine;
+use pasta_curves::{Ep, EpAffine, Fp};
 
 fn multiexp_gpu<Q, D, G, S>(
     pool: &Worker,
@@ -43,14 +44,16 @@ fn gpu_multiexp_consistency() {
         .map(|device| crate::program!(device))
         .collect::<Result<_, _>>()
         .expect("Cannot create programs!");
-    let mut kern = MultiexpKernel::<<Bls12 as Engine>::G1Affine>::create(programs, &devices)
-        .expect("Cannot initialize kernel!");
+    //let mut kern = MultiexpKernel::<<Bls12 as Engine>::G1Affine>::create(programs, &devices)
+    let mut kern =
+        MultiexpKernel::<EpAffine>::create(programs, &devices).expect("Cannot initialize kernel!");
     let pool = Worker::new();
 
     let mut rng = rand::thread_rng();
 
     let mut bases = (0..(1 << START_LOG_D))
-        .map(|_| <Bls12 as Engine>::G1::random(&mut rng).to_affine())
+        //.map(|_| <Bls12 as Engine>::G1::random(&mut rng).to_affine())
+        .map(|_| Ep::random(&mut rng).to_affine())
         .collect::<Vec<_>>();
 
     for log_d in START_LOG_D..=MAX_LOG_D {
@@ -61,7 +64,8 @@ fn gpu_multiexp_consistency() {
 
         let v = Arc::new(
             (0..samples)
-                .map(|_| <Bls12 as Engine>::Fr::random(&mut rng).to_repr())
+                //.map(|_| <Bls12 as Engine>::Fr::random(&mut rng).to_repr())
+                .map(|_| Fp::random(&mut rng).to_repr())
                 .collect::<Vec<_>>(),
         );
 
